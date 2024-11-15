876552e90a828264beddf4cea24c3e6add86f155
M. Isa Gul <71321835+misagul@users.noreply.github.com>
2024-11-15 09:58:29 +0300

    change ns

--- killercoda/setup.sh
+++ killercoda/setup.sh
@@ -4,6 +4,6 @@ kubectl apply -f volume.yaml
 kubectl apply -f deployment.yaml
 kubectl apply -f service.yaml
 echo "Waiting for the pod to be ready..."
-kubectl wait --for=condition=Ready --timeout=-1s $(kubectl get pods -n devops-tools -o name | grep '^pod/jenkins') -n devops-tools
-echo port: $(kubectl get svc jenkins-service -n devops-tools -o jsonpath="{.spec.ports[0].nodePort}")
-echo password: $(kubectl exec -it $(kubectl get pods -n devops-tools -o name | grep '^pod/jenkins') -n devops-tools -- cat /var/jenkins_home/secrets/initialAdminPassword)
+kubectl wait --for=condition=Ready --timeout=-1s $(kubectl get pods -n jenkins -o name | grep '^pod/jenkins') -n devops-tools
+echo port: $(kubectl get svc jenkins-service -n jenkins -o jsonpath="{.spec.ports[0].nodePort}")
+echo password: $(kubectl exec -it $(kubectl get pods -n jenkins -o name | grep '^pod/jenkins') -n jenkins -- cat /var/jenkins_home/secrets/initialAdminPassword)
